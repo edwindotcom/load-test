d9fe17ad3cc890848789af056a40ac4a157d836e
Edwin Wong <edwong@mozilla.com>
2013-07-26 14:13:39 -0700

    Add put timing, timeout, fix strings

--- loads/pushgo/README.md
+++ loads/pushgo/README.md
@@ -3,7 +3,7 @@ Load test for jrconlin/pushgo. Runs types of tests:
 * connect, hello, register, update, close
 * connect, hello, register, update loop one channel, ack, close
 * connect, hello, register, update loop different channel, ack, close
-* connect, hello, register, update, ack, ping loop, close
+* connect, hello, ping loop, close
 
 You can run this by installing Loads (mozilla-services/loads) and running this:
-loads-runner load_test.load_gen.TestLoad.test_load -c 10 -u 10
+loads-runner load_gen.TestLoad.test_load -c 10 -u 10
--- loads/pushgo/load_gen.py
+++ loads/pushgo/load_gen.py
@@ -19,10 +19,12 @@ from pushtest.utils import (
 
 TARGET_SERVER = "ws://ec2-54-244-206-75.us-west-2.compute.amazonaws.com:8080"
 # TARGET_SERVER = "ws://localhost:8080"
-VERBOSE = True
 
-def _log(txt):
-    if VERBOSE:
+# either error or debug
+LOG_LEVEL = 'error' 
+
+def _log(txt, level = 'debug'):
+    if LOG_LEVEL == level:
         print '::', txt
 
 
@@ -36,15 +38,17 @@ class WsClient(WebSocketClient):
     uaid = ""
     version = 0
     count = 0
-    sleep = 0
+    sleep = 20
 
     reg_time = 0
-    put_time = 0
+    put_start = 0
+    put_end = 0
 
     client_type = ""
-    max_sleep = 1
-    max_updates = 5
-    timeout = 20 
+    max_sleep = 21
+    max_updates = 10
+    timeout = 30
+    last_time = 0
 
     client_types = {'conn_close': 30,
                     'conn_noack': 5,
@@ -53,7 +57,8 @@ class WsClient(WebSocketClient):
                     'ping_loop': 5}
 
     def opened(self):
-        self.client_type = get_prob(self.client_types)
+        self.client_type = 'ping_loop'
+        #self.client_type = get_prob(self.client_types)
         _log(self.client_type)
 
         self.sleep = get_rand(self.max_sleep)
@@ -61,12 +66,15 @@ class WsClient(WebSocketClient):
         self.uaid = get_uaid()
         self.version = int(str_gen(8))
         self.start_time = time.time()
+        self.last_time = self.start_time
 
         self.hello()
 
     def closed(self, code, reason=None):
-        print('\nTime to register: %s s' % (self.reg_time - self.start_time))
-        print('Time to notification: %s s' % (self.put_end - self.put_start))
+        if self.client_type != 'ping_loop':
+            _log('\nTime to register: %s s' % (self.reg_time - self.start_time))
+            _log('Time to notification: %s s' % (self.put_end - self.put_start))
+
         _log("Closed down: %s %s" % (code, reason))
 
     def hello(self):
@@ -97,7 +105,7 @@ class WsClient(WebSocketClient):
     def check_response(self, data):
         if "status" in data.keys():
             if data['status'] != 200:
-                _log('ERROR status: %s' % data['status'])
+                _log('ERROR status: %s' % data['status'], 'error')
                 self.close()
 
     def new_chan(self):
@@ -105,23 +113,32 @@ class WsClient(WebSocketClient):
         self.version = int(str_gen(8))
         self.hello()
 
+    def check_timeout(self):
+        _log(time.time() - self.last_time)
+        if time.time() > self.last_time + float(self.timeout):
+            _log('\nTIMEOUT: %s seconds' % self.timeout, 'error')
+            self.close()
+        else:
+            self.last_time = time.time()
+
     def received_message(self, m):
         data = json.loads(m.data)
         self.check_response(data)
+        self.check_timeout()
 
         _log(data)
 
         if self.count > self.max_updates:
-            self.close()
-        elif time.time() > self.start_time + float(self.timeout):
-            _log('TIMEOUT: %s seconds' % self.timeout)
             self.close()
         else:
             if "messageType" in data:
                 time.sleep(self.sleep)
 
                 if data["messageType"] == "hello":
-                    self.reg()
+                    if self.client_type == 'ping_loop':
+                        self.ping()
+                    else:
+                        self.reg()
                 if data["messageType"] == "register":
                     self.reg_time = time.time()
                     self.endpoint = data["pushEndpoint"]
@@ -156,10 +173,10 @@ class TestLoad(TestCase):
     - connect, hello, register, update, close
     - connect, hello, register, update loop one channel, ack, close
     - connect, hello, register, update loop different channel, ack, close
-    - connect, hello, register, update, ack, ping loop, close
+    - ping_loop: connect, hello, ping loop, close
 
     You can run this by installing Loads and running this:
-    loads-runner load_test.load_gen.TestLoad.test_load -c 10 -u 10
+    loads-runner load_gen.TestLoad.test_load -c 10 -u 10
     """
 
     def test_load(self):
